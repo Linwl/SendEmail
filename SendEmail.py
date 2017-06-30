dc204cf3c0b1be14e23644fd52aaccfe4d85d487
Linwl <304115325@qq.com>
2017-06-30 10:20:38 +0800

    增加发送电子邮件模块

---
+++ SendEmail.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+_user = "你的qq邮箱"
+_pwd  = "你的授权码"
+_to   = "接收的邮箱"
+
+msg = MIMEText("Hello world!")
+msg["Subject"] = "测试"
+msg["From"]    = _user
+msg["To"]      = _to
+
+try:
+    s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+    s.login(_user, _pwd)
+    s.sendmail(_user, _to, msg.as_string())
+    s.quit()
+    print "Success!"
+except smtplib.SMTPException,e:
+    print "Falied,%s"%e
